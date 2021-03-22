ffa2f8134b033b7eb6dbd62da287b9732f5d4282
Carlos <carloscollado2001@gmail.com>
2021-03-22 10:46:21 -0400

    first commit

---
+++ Assets/Script/ShipMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour
+{
+
+    bool ifTouched;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchposition = Camera.main.ScreenToWorldPoint(touch.position);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                ifTouched = true;
+            }
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+                if (ifTouched)
+                {
+                    transform.position = new Vector2(0, touchposition.y);
+                }
+            }
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+                ifTouched = false;
+            }
+        }
+    }
+}
